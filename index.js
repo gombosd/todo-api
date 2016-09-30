3d3907a5952c8178d7197a0fbb9a161f842125b2
gombosd <gombos.david96@gmail.com>
2016-09-30 13:26:45 +0200

    done dev env

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
-  res.send('más bármi');
+  var log = {
+  	headers: req.headers
+  };
+  res.send(log);
+});
+
+app.get('/:id', function (req, res) {
+  var log = {
+  	headers: req.headers,
+  	id: req.params.id,
+  	query: req.query
+  };
+  res.send(log);
+});
+
+app.post('/', function (req, res) {
+  res.json(req.body);
 });
 
 app.listen(3000, function () {
