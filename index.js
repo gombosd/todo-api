643271a99aa3620bbc13e8ad7152f447b9bc39da
gombosd <gombos.david96@gmail.com>
2016-10-10 16:17:11 +0200

    heroku install

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use('/*',function(req, res){
 });
 
 //app listener
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on port 3000!');
 });
